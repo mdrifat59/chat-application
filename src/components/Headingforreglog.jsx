2f5041e7df73f2bf2656f556c9338fcfae1968ef
mdrifat59 <mdrifatulislam59@gmail.com>
2024-02-18 13:05:05 +0600

    font-family add

---
+++ src/components/Headingforreglog.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const headingforreglog = ({title,className}) => {
+  return (
+    <h3 className={className}> {title}</h3>
+  )
+}
+
+export default headingforreglog
--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Grid } from '@mui/material'
-import registrationimg from '../assets/registrationimg.png'
+import registrationimg from '../assets/registrationimg.png' 
+import Headingforreglog from '../components/Headingforreglog'
 
 const Registration = () => {
   return (
     <Grid container spacing={2}>
   <Grid item xs={6}>
-    <h1>xs=8</h1>
+      <Headingforreglog className="headingreglog" title="Get started with easily register"/>
   </Grid>
   <Grid item xs={6}>
     <img className='regiimg' src={registrationimg} />
