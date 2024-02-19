c13de84fdf657f9d8ee2e7144898b826d898149a
mdrifat59 <mdrifatulislam59@gmail.com>
2024-02-19 19:26:58 +0600

    registration input field done

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Grid } from '@mui/material'
+import { Grid,TextField } from '@mui/material'
 import registrationimg from '../assets/registrationimg.png' 
 import Headingforreglog from '../components/Headingforreglog'
 
@@ -9,6 +9,16 @@ const Registration = () => {
   <Grid item xs={6}>
     <div className='regcontainer'> 
       <Headingforreglog className="headingreglog" title="Get started with easily register"/>
+      <p>Free register and you can enjoy it</p>
+      <div className='reginput'> 
+      <TextField id="outlined-basic" label="Email Address" variant="outlined" />
+      </div>
+      <div className='reginput'>
+      <TextField id="outlined-basic" label="Full Name" variant="outlined" />
+      </div>
+      <div className='reginput'>
+      <TextField id="outlined-basic" label="Password" variant="outlined" />
+      </div>
     </div>
   </Grid>
   <Grid item xs={6}>
