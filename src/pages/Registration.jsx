e09a390a1b47bfd66a317324671b6590132deae9
mdrifat59 <mdrifatulislam59@gmail.com>
2024-02-21 13:05:53 +0600

    registration data up to firebase server connection done

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Grid,TextField,Button } from '@mui/material'
 import registrationimg from '../assets/registrationimg.png' 
 import Headingforreglog from '../components/Headingforreglog'
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+let initialvalues ={
+  email:"",
+ fullName:"",
+ password:""
+}
 
 const Registration = () => {
+
+  const auth = getAuth();
+  let [values,setValues]=useState(initialvalues)
+
+  let handleValues = (e)=>{
+        setValues({
+          ...values,
+          [e.target.name]: e.target.value
+        })
+  }
+  let handleSubmit=()=>{
+      let {email, fullName, password}=values
+      createUserWithEmailAndPassword(auth,email,password).then((user)=>{
+            console.log(user)
+      })
+  }
   return (
     <Grid container spacing={2}>
   <Grid item xs={6}>
@@ -11,15 +34,15 @@ const Registration = () => {
       <Headingforreglog className="headingreglog" title="Get started with easily register"/>
       <p>Free register and you can enjoy it</p>
       <div className='reginput'> 
-      <TextField id="outlined-basic" label="Email Address" variant="outlined" />
+      <TextField onChange={handleValues} name='email' id="outlined-basic" label="Email Address" variant="outlined" />
       </div>
       <div className='reginput'>
-      <TextField id="outlined-basic" label="Full Name" variant="outlined" />
+      <TextField onChange={handleValues} name='fullName' id="outlined-basic" label="Full Name" variant="outlined" />
       </div>
       <div className='reginput'>
-      <TextField id="outlined-basic" label="Password" variant="outlined" />
+      <TextField type='password' onChange={handleValues} name='password' id="outlined-basic" label="Password" variant="outlined" />
       </div> 
-      <Button className='regbutton' variant="contained">Sing up</Button> 
+      <Button onClick={handleSubmit} className='regbutton' variant="contained">Sing up</Button> 
     </div>
   </Grid>
   <Grid item xs={6}>
