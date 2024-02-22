2601a735c7797372e08de70f79455279a0a86faf
mdrifat59 <mdrifatulislam59@gmail.com>
2024-02-22 10:50:08 +0600

    login page data checking in firebase registration data complete

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Grid,TextField,Button } from '@mui/material'
+import LoadingButton from '@mui/lab/LoadingButton';
 import login from '../assets/login.jpg' 
 import google from '../assets/google.png' 
 import Headingforreglog from '../components/Headingforreglog'
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth"; 
+
+let initialvalues ={
+  email:"", 
+ password:"",
+ loading:false
+}
+
 
 const Login = () => {
+  const auth = getAuth();
+  let [values,setValues]=useState(initialvalues)
+  let handleValues = (e)=>{
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value
+    })
+  }
+  let handleSubmit=()=>{
+    let {email, fullName, password}=values
+    setValues({
+      ...values,
+       loagding: true
+    })
+    signInWithEmailAndPassword(auth,email,password).then((user)=>{
+          console.log(user)
+          setValues({
+            email:"", 
+            password:"",
+             loagding: false
+          }) 
+          console.log(user)
+    })
+  }
   return (
     <Grid container spacing={2}>
     <Grid item xs={6}>
@@ -12,12 +45,19 @@ const Login = () => {
         <Headingforreglog className="headingreglog" title="Login to your account!"/> 
         <img className='google' src={google} />
         <div className='reginput'> 
-        <TextField id="outlined-basic" label="Email Address" variant="outlined" />
+        <TextField value={values.email} onChange={handleValues} name='email' id="outlined-basic" label="Email Address" variant="outlined" />
         </div> 
         <div className='reginput'>
-        <TextField id="outlined-basic" label="Password" variant="outlined" />
+        <TextField type='password' value={values.password} onChange={handleValues} name='password' id="outlined-basic" label="Password" variant="outlined" />
         </div> 
-        <Button className='loginbutton' variant="contained">Login to Continue</Button> 
+        {values.loading
+      ?
+      <LoadingButton loading variant="outlined">
+        Submit
+      </LoadingButton>
+      :
+      <Button onClick={handleSubmit} className='regbutton' variant="contained">Login to Continue</Button> 
+    } 
       </div>
     </Grid>
     <Grid item xs={6}>
