6a231bb8f69f0bdd2d5cebb54a6575f8cb48281f
mdrifat59 <mdrifatulislam59@gmail.com>
2024-02-27 19:33:42 +0600

    RootLayout page dividete in grid

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Grid from '@mui/material/Grid';
 
 const RootLayout = () => {
   return (
     <>
-        <div>RootLayout</div>
+         <Grid container spacing={2}>
+        <Grid item xs={2}>
+          <h1>Navbar</h1>
+        </Grid>
+        <Grid item xs={10}> 
           <Outlet/>
+        </Grid> 
+      </Grid>
+
     </>
   )
 }
