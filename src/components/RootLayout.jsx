5d4e53fbe5a32b1f4fdba7a23efa67950e47ee38
mdrifat59 <mdrifatulislam59@gmail.com>
2024-02-28 00:34:19 +0600

    Rootlayout icon is done

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Grid from '@mui/material/Grid';
-import profile from '../assets/profile.png'  
+import profile from '../assets/profile.png'   
+import { AiOutlineMessage } from "react-icons/ai";
+import { IoIosNotificationsOutline,IoMdSettings,IoIosLogOut,IoIosHome    } from "react-icons/io";
+
 
 const RootLayout = () => {
   return (
@@ -10,12 +13,13 @@ const RootLayout = () => {
         <Grid item xs={1}>
             <div className='navbar'>
               <div className="navcontainer">
-                <img src={profile} alt="" />
+                <img src={profile} />
                 <ul>
-                  <li>one</li>
-                  <li>one</li>
-                  <li>one</li>
-                  <li>one</li>
+                  <li><IoIosHome  className='icon' /></li>       
+                  <li><AiOutlineMessage className='icon' /></li>       
+                  <li><IoIosNotificationsOutline className='icon' /></li>       
+                  <li><IoMdSettings className='icon' /></li>     
+                  <li><IoIosLogOut className='icon' /></li>     
                 </ul>
               </div>
             </div>
