56c8628519ee893dcbfe2fe09cd581dcacf7e2fe
mdrifat59 <mdrifatulislam59@gmail.com>
2024-02-29 11:17:33 +0600

    home page Dividet by gird and Group component create and connection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
-import { Button } from '@mui/material'
+import { Button,Grid } from '@mui/material'
 import React from 'react'
 import { getAuth, signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import Group from '../components/Group';
 
 const Home = () => {
     const auth = getAuth();
@@ -14,7 +15,20 @@ const Home = () => {
           });
     }
   return (
-    <Button onClick={handleLogOut} variant='contained'>Log Out</Button>
+    
+    //  <Button onClick={handleLogOut} variant='contained'>Log Out</Button> 
+    <Grid container spacing={2}>
+  <Grid item xs={4}>
+     <Group/>
+  </Grid>
+  <Grid item xs={4}>
+    <h1>xs=4</h1>
+  </Grid>
+  <Grid item xs={4}>
+    <h1>xs=4</h1>
+  </Grid> 
+</Grid>
+    
   )
 }
 
