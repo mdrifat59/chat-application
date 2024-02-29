f003de9094f423bde70bb063235dcd9fc5c87ccb
mdrifat59 <mdrifatulislam59@gmail.com>
2024-02-29 11:55:55 +0600

    Group component is done

--- src/components/Group.jsx
+++ src/components/Group.jsx
@@ -1,8 +1,134 @@
 import React from 'react'
+import GroupImg from '../assets/group.png'
+import {Button} from '@mui/material';
 
 const Group = () => {
   return (
-    <div>Group</div>
+    <div className='box'> 
+        <h3 className='title'>Group List
+        <Button size="small" variant="contained">Join</Button>
+        </h3>
+        <div className="list">
+            <div className="img">
+                <img src={GroupImg} alt="" />
+            </div>
+            <div className="details">
+                <h4>Friends Reunion</h4>
+                <p>Hi Guys, Wassup!</p>
+            </div>
+            <div className="button">
+            <Button size="small" variant="contained">Join</Button>
+            </div>
+        </div>
+        <div className="list">
+            <div className="img">
+                <img src={GroupImg} alt="" />
+            </div>
+            <div className="details">
+                <h4>Friends Reunion</h4>
+                <p>Hi Guys, Wassup!</p>
+            </div>
+            <div className="button">
+            <Button size="small" variant="contained">Join</Button>
+            </div>
+        </div>
+        <div className="list">
+            <div className="img">
+                <img src={GroupImg} alt="" />
+            </div>
+            <div className="details">
+                <h4>Friends Reunion</h4>
+                <p>Hi Guys, Wassup!</p>
+            </div>
+            <div className="button">
+            <Button size="small" variant="contained">Join</Button>
+            </div>
+        </div>
+        <div className="list">
+            <div className="img">
+                <img src={GroupImg} alt="" />
+            </div>
+            <div className="details">
+                <h4>Friends Reunion</h4>
+                <p>Hi Guys, Wassup!</p>
+            </div>
+            <div className="button">
+            <Button size="small" variant="contained">Join</Button>
+            </div>
+        </div>
+        <div className="list">
+            <div className="img">
+                <img src={GroupImg} alt="" />
+            </div>
+            <div className="details">
+                <h4>Friends Reunion</h4>
+                <p>Hi Guys, Wassup!</p>
+            </div>
+            <div className="button">
+            <Button size="small" variant="contained">Join</Button>
+            </div>
+        </div>
+        <div className="list">
+            <div className="img">
+                <img src={GroupImg} alt="" />
+            </div>
+            <div className="details">
+                <h4>Friends Reunion</h4>
+                <p>Hi Guys, Wassup!</p>
+            </div>
+            <div className="button">
+            <Button size="small" variant="contained">Join</Button>
+            </div>
+        </div>
+        <div className="list">
+            <div className="img">
+                <img src={GroupImg} alt="" />
+            </div>
+            <div className="details">
+                <h4>Friends Reunion</h4>
+                <p>Hi Guys, Wassup!</p>
+            </div>
+            <div className="button">
+            <Button size="small" variant="contained">Join</Button>
+            </div>
+        </div>
+        <div className="list">
+            <div className="img">
+                <img src={GroupImg} alt="" />
+            </div>
+            <div className="details">
+                <h4>Friends Reunion</h4>
+                <p>Hi Guys, Wassup!</p>
+            </div>
+            <div className="button">
+            <Button size="small" variant="contained">Join</Button>
+            </div>
+        </div>
+        <div className="list">
+            <div className="img">
+                <img src={GroupImg} alt="" />
+            </div>
+            <div className="details">
+                <h4>Friends Reunion</h4>
+                <p>Hi Guys, Wassup!</p>
+            </div>
+            <div className="button">
+            <Button size="small" variant="contained">Join</Button>
+            </div>
+        </div>
+        <div className="list">
+            <div className="img">
+                <img src={GroupImg} alt="" />
+            </div>
+            <div className="details">
+                <h4>Friends Reunion</h4>
+                <p>Hi Guys, Wassup!</p>
+            </div>
+            <div className="button">
+            <Button size="small" variant="contained">Join</Button>
+            </div>
+        </div>
+    </div>
   )
 }
 
