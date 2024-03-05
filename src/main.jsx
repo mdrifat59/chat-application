6d6f5766e21b94f02338d0d76662e8f2f8ed067f
mdrifat59 <mdrifatulislam59@gmail.com>
2024-03-05 23:48:58 +0600

    React Redux install and setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import firebaseConfig from './FirebaseConfig.jsx'
+// import firebaseConfig from './FirebaseConfig.jsx'
 import App from './App.jsx'
 import './index.css'
+import store from './app/store'
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
 )
