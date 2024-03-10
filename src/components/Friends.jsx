c9aa36cc28d0b52e98dff17a16a3fbf83fef7e97
mdrifat59 <mdrifatulislam59@gmail.com>
2024-03-10 12:37:17 +0600

    Friends componenet is done

--- src/components/Friends.jsx
+++ src/components/Friends.jsx
@@ -1,130 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import GroupImg from '../assets/group.png'  
+import { getDatabase, ref, onValue } from "firebase/database";
+import { useSelector } from 'react-redux';
+import {Button} from '@mui/material';
+
 
 const Friends = () => {
+    const db = getDatabase();
+    let userData=useSelector((state)=>state.loggedUser.loginuser)
+    let [frieds, setFriends]=useState([])
+    useEffect(()=>{
+        const friendsRef = ref(db, 'friends/' );
+        onValue(friendsRef, (snapshot) => {
+             let arr =[]
+             snapshot.forEach(item=>{
+                if(item.val().whosendid == userData.uid || item.val().whoreceiveid == userData.uid){
+                    arr.push({...item.val(), id:item.key})
+                }
+             })
+             setFriends(arr);
+},[ ]);
+    
+    })
   return (
     <div className='box'> 
     <h3>Friends </h3>
+    {frieds.map(item=>(
     <div className="list">
         <div className="img">
             <img src={GroupImg} alt="" />
         </div>
         <div className="details">
-            <h4>Friends Reunion</h4>
+             {item.whoreceiveid == userData.uid
+             ?
+             <h4>{item.whosendname}</h4>
+             :
+             <h4>{item.whoreceivename}</h4>
+        }
             <p>Hi Guys, Wassup!</p>
         </div>
         <div className="button">
          <p>Today, 8:56pm</p>
         </div>
-    </div>
-    <div className="list">
-        <div className="img">
-            <img src={GroupImg} alt="" />
-        </div>
-        <div className="details">
-            <h4>Friends Reunion</h4>
-            <p>Hi Guys, Wassup!</p>
-        </div>
         <div className="button">
-        <p>Today, 8:56pm</p>
-        </div>
+            <Button onClick={()=>handleDelete(item.id)} size="small" variant="contained" color="error">Block</Button>
+            </div>
     </div>
-    <div className="list">
-        <div className="img">
-            <img src={GroupImg} alt="" />
-        </div>
-        <div className="details">
-            <h4>Friends Reunion</h4>
-            <p>Hi Guys, Wassup!</p>
-        </div>
-        <div className="button">
-        <p>Today, 8:56pm</p>
-        </div>
-    </div>
-    <div className="list">
-        <div className="img">
-            <img src={GroupImg} alt="" />
-        </div>
-        <div className="details">
-            <h4>Friends Reunion</h4>
-            <p>Hi Guys, Wassup!</p>
-        </div>
-        <div className="button">
-        <p>Today, 8:56pm</p>
-        </div>
-    </div>
-    <div className="list">
-        <div className="img">
-            <img src={GroupImg} alt="" />
-        </div>
-        <div className="details">
-            <h4>Friends Reunion</h4>
-            <p>Hi Guys, Wassup!</p>
-        </div>
-        <div className="button">
-        <p>Today, 8:56pm</p>
-        </div>
-    </div>
-    <div className="list">
-        <div className="img">
-            <img src={GroupImg} alt="" />
-        </div>
-        <div className="details">
-            <h4>Friends Reunion</h4>
-            <p>Hi Guys, Wassup!</p>
-        </div>
-        <div className="button">
-        <p>Today, 8:56pm</p>
-        </div>
-    </div>
-    <div className="list">
-        <div className="img">
-            <img src={GroupImg} alt="" />
-        </div>
-        <div className="details">
-            <h4>Friends Reunion</h4>
-            <p>Hi Guys, Wassup!</p>
-        </div>
-        <div className="button">
-        <p>Today, 8:56pm</p>
-        </div>
-    </div>
-    <div className="list">
-        <div className="img">
-            <img src={GroupImg} alt="" />
-        </div>
-        <div className="details">
-            <h4>Friends Reunion</h4>
-            <p>Hi Guys, Wassup!</p>
-        </div>
-        <div className="button">
-        <p>Today, 8:56pm</p>
-        </div>
-    </div>
-    <div className="list">
-        <div className="img">
-            <img src={GroupImg} alt="" />
-        </div>
-        <div className="details">
-            <h4>Friends Reunion</h4>
-            <p>Hi Guys, Wassup!</p>
-        </div>
-        <div className="button">
-        <p>Today, 8:56pm</p>
-        </div>
-    </div>
-    <div className="list">
-        <div className="img">
-            <img src={GroupImg} alt="" />
-        </div>
-        <div className="details">
-            <h4>Friends Reunion</h4>
-            <p>Hi Guys, Wassup!</p>
-        </div>
-        <div className="button">
-        <p>Today, 8:56pm</p>
-        </div>
-    </div>
+    ))}
+     
 </div>
   )
 }
