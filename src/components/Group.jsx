3825cf08f5a562371cb275a2c3f786475ba4de02
mdrifat59 <mdrifatulislam59@gmail.com>
2024-03-15 11:00:29 +0600

    Modal add in Group component

--- src/components/Group.jsx
+++ src/components/Group.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
-import GroupImg from '../assets/group.png'
-import {Button} from '@mui/material';
+import {React,useState} from 'react'
+import GroupImg from '../assets/group.png'  
+import {Button, Typography, Modal, Box, TextField  } from '@mui/material'; 
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
 
 const Group = () => {
+    const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
     <div className='box'> 
         <h3 className='title'>Group List
-        <Button size="small" variant="contained">Join</Button>
+        <Button size="small" variant="contained"  onClick={handleOpen}>Join</Button>
+        <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Creat your group
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+          <TextField margin="dense"  id="outlined-basic" label="Group Name" variant="outlined" />
+          <TextField id="outlined-basic" margin="dense"  label="Group Tagline" variant="outlined" />
+          </Typography>
+        </Box>
+      </Modal>
         </h3>
         <div className="list">
             <div className="img">
