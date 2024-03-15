594ac4db183d73aa81fe949195941e3e70a7b2b5
mdrifat59 <mdrifatulislam59@gmail.com>
2024-03-15 17:01:23 +0600

    if current user create group then group show in My Group Component

--- src/components/MyGroups.jsx
+++ src/components/MyGroups.jsx
@@ -1,132 +1,51 @@
-import React from 'react'
-import GroupImg from '../assets/group.png' 
+import React, { useEffect, useState } from 'react'
+import GroupImg from '../assets/group.png'
+import { getDatabase, ref, onValue } from "firebase/database";
+import { useSelector } from 'react-redux';
 
 const MyGroups = () => {
-  return (
-    <div className='box'> 
-    <h3>My Groups</h3>
-    <div className="list">
-        <div className="img">
-            <img src={GroupImg} alt="" />
+    const db = getDatabase();
+    let [group, setGroup]=useState([])
+    let userData = useSelector((state) => state.loggedUser.loginuser)
+    useEffect(() => {
+        const groupRef = ref(db, 'groups/');
+        let arr = []
+        onValue(groupRef, (snapshot) => {
+            snapshot.forEach(item=>{
+                if(item.val().adminid == userData.uid){
+                    arr.push({...item.val(), id:item.key})
+                }
+            })
+            setGroup(arr)
+        });
+    },[])
+    return (
+        <div className='box'>
+            <h3>My Groups</h3>
+            {group.length == 0
+            ?
+                <h2>your not group yet</h2>
+            :
+            
+            group.map(item=>(
+
+            <div className="list">
+                <div className="img">
+                    <img src={GroupImg} alt="" />
+                </div>
+                <div className="details">
+                    <h4>{item.groupname}</h4>
+                    <p>{item.grouptagline}</p>
+                </div>
+                <div className="button">
+                    <p>Today, 8:56pm</p>
+                </div>
+            </div>
+            ))
+            }
+
         </div>
-        <div className="details">
-            <h4>Friends Reunion</h4>
-            <p>Hi Guys, Wassup!</p>
-        </div>
-        <div className="button">
-         <p>Today, 8:56pm</p>
-        </div>
-    </div>
-    <div className="list">
-        <div className="img">
-            <img src={GroupImg} alt="" />
-        </div>
-        <div className="details">
-            <h4>Friends Reunion</h4>
-            <p>Hi Guys, Wassup!</p>
-        </div>
-        <div className="button">
-        <p>Today, 8:56pm</p>
-        </div>
-    </div>
-    <div className="list">
-        <div className="img">
-            <img src={GroupImg} alt="" />
-        </div>
-        <div className="details">
-            <h4>Friends Reunion</h4>
-            <p>Hi Guys, Wassup!</p>
-        </div>
-        <div className="button">
-        <p>Today, 8:56pm</p>
-        </div>
-    </div>
-    <div className="list">
-        <div className="img">
-            <img src={GroupImg} alt="" />
-        </div>
-        <div className="details">
-            <h4>Friends Reunion</h4>
-            <p>Hi Guys, Wassup!</p>
-        </div>
-        <div className="button">
-        <p>Today, 8:56pm</p>
-        </div>
-    </div>
-    <div className="list">
-        <div className="img">
-            <img src={GroupImg} alt="" />
-        </div>
-        <div className="details">
-            <h4>Friends Reunion</h4>
-            <p>Hi Guys, Wassup!</p>
-        </div>
-        <div className="button">
-        <p>Today, 8:56pm</p>
-        </div>
-    </div>
-    <div className="list">
-        <div className="img">
-            <img src={GroupImg} alt="" />
-        </div>
-        <div className="details">
-            <h4>Friends Reunion</h4>
-            <p>Hi Guys, Wassup!</p>
-        </div>
-        <div className="button">
-        <p>Today, 8:56pm</p>
-        </div>
-    </div>
-    <div className="list">
-        <div className="img">
-            <img src={GroupImg} alt="" />
-        </div>
-        <div className="details">
-            <h4>Friends Reunion</h4>
-            <p>Hi Guys, Wassup!</p>
-        </div>
-        <div className="button">
-        <p>Today, 8:56pm</p>
-        </div>
-    </div>
-    <div className="list">
-        <div className="img">
-            <img src={GroupImg} alt="" />
-        </div>
-        <div className="details">
-            <h4>Friends Reunion</h4>
-            <p>Hi Guys, Wassup!</p>
-        </div>
-        <div className="button">
-        <p>Today, 8:56pm</p>
-        </div>
-    </div>
-    <div className="list">
-        <div className="img">
-            <img src={GroupImg} alt="" />
-        </div>
-        <div className="details">
-            <h4>Friends Reunion</h4>
-            <p>Hi Guys, Wassup!</p>
-        </div>
-        <div className="button">
-        <p>Today, 8:56pm</p>
-        </div>
-    </div>
-    <div className="list">
-        <div className="img">
-            <img src={GroupImg} alt="" />
-        </div>
-        <div className="details">
-            <h4>Friends Reunion</h4>
-            <p>Hi Guys, Wassup!</p>
-        </div>
-        <div className="button">
-        <p>Today, 8:56pm</p>
-        </div>
-    </div>
-</div>
-  )
+    )
 }
 
 export default MyGroups
