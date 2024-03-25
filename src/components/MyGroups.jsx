69618ac7f33208752655e603aefa9072e3055942
mdrifat59 <mdrifatulislam59@gmail.com>
2024-03-25 12:38:48 +0600

    add modal in MyGroup component

--- src/components/MyGroups.jsx
+++ src/components/MyGroups.jsx
@@ -3,10 +3,29 @@ import GroupImg from '../assets/group.png'
 import { getDatabase, ref, onValue } from "firebase/database";
 import { useSelector } from 'react-redux';
 import { Button } from '@mui/material';
+import Box from '@mui/material/Box'; 
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
 
 const MyGroups = () => {
     const db = getDatabase();
     let [myGroup, setMyGroup] = useState([])
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
     let userData = useSelector((state) => state.loggedUser.loginuser)
     useEffect(() => {
         const groupRef = ref(db, 'groups/');
@@ -41,7 +60,7 @@ const MyGroups = () => {
                         <p>{item.grouptagline}</p> 
                     </div>
                     <div className="button">
-                        <Button size="small" variant="contained">Request</Button>
+                        <Button onClick={handleOpen} size="small" variant="contained">Request</Button>
                     </div>
                     <div className="button">
                         <Button size="small" variant="contained">Member</Button>
@@ -49,6 +68,22 @@ const MyGroups = () => {
                 </div>
             ))
             }
+            {/* modal */}
+            <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
 
         </div>
     )
