cdaca8f4919ef5f49790cae2c11805577d13e54d
mdrifat59 <mdrifatulislam59@gmail.com>
2024-04-03 16:19:59 +0600

    Message page layout is done

--- src/pages/Message.jsx
+++ src/pages/Message.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Grid } from '@mui/material'
+import Group from '../components/Group';
+import FriendRequest from '../components/FriendRequest';
+import Friends from '../components/Friends';
+import MyGroups from '../components/MyGroups';
+import UserList from '../components/UserList';
+import Block from '../components/Block';
 
 const Message = () => {
   return (
-    <div>Message</div>
+    <Grid container spacing={2}>
+    <Grid item xs={4}>
+       <Group/>
+       <Friends/>       
+    </Grid>
+    <Grid item xs={8}>
+      
+    </Grid> 
+  </Grid>
   )
 }
 
