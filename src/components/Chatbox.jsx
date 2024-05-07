35c8c9b3cc2946572044917623fc00fb507eec61
mdrifat59 <mdrifatulislam59@gmail.com>
2024-05-07 23:50:42 +0600

    moment.js install and chatbox inpute date upto firebase database

--- src/components/Chatbox.jsx
+++ src/components/Chatbox.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import profile from '../assets/profile.png'
 import registrationimg from '../assets/registrationimg.png'
 import ModalImage from "react-modal-image";
 import Button from '@mui/material/Button';
 import { useSelector } from 'react-redux';
+import { getDatabase, ref, set, push } from "firebase/database";
+import moment from 'moment';
+
 const Chatbox = () => {
+  const db = getDatabase();
   let activeChat = useSelector((state) => state.activeChat.activeChat)
+  let userData = useSelector((state) => state.loggedUser.loginuser)
+  let [msg, setMsg]=useState("")
+
+  let handleChat = ()=>{ 
+    if(activeChat.type == "groupmsg"){
+      console.log("groupmsg")
+    }else{
+      if(msg != ""){
+        set(push(ref(db, 'singlemsg/')), {
+          whosendname: userData.displayName,
+          whosendid:userData.uid,
+          whorecivename:activeChat.name,
+          whoreciveid:activeChat.id,
+          msg:msg,
+          date:`${new Date().getFullYear()}-${new Date().getMonth() +1}-${new Date().getDate()} ${new Date().getHours()}:${new Date().getMinutes()}`,
+        });
+      }
+    } 
+  }
   return (
     <div className='chatbox'>
         <div className='msgprofile'>
@@ -42,7 +65,7 @@ const Chatbox = () => {
               <p className='getaudio'>
                 <audio controls></audio>
               </p>
-              <p className='time'>Today, 2:01pm</p>
+              <p className='time'>{moment("2024-5-7 23:35", "YYYYMMDD h:mm").fromNow()}</p>
             </div>
             <div className='msg'> 
               <p className='sendaudio'>
@@ -105,9 +128,9 @@ const Chatbox = () => {
         </div>
         <div className='msgcontainer'>
           <div className='msgwritecon' >
-          <input type="text" className='msgwrite' />
+          <input onChange={(e)=>setMsg(e.target.value)} type="text" className='msgwrite' />
           </div>
-          <Button variant="contained">send</Button>
+          <Button variant="contained" onClick={handleChat}>send</Button>
         </div>
     </div>
   )
